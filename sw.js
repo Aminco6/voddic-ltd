d58ca5f3b0a8b4f4e99fae2bace57297ed955445
Oluwasanmi Israel-collins <148803978+Aminco6@users.noreply.github.com>
2026-01-19 11:49:35 +0100

    Update service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("voddic-esp").then(cache =>
+    caches.open("voddic-esp-app").then(cache =>
       cache.addAll([
-        "/",
-        "/index.html",
         "/app.html",
-        "/manifest.json"
+        "/manifest.json",
+        "/assets/images/resources/voddic-ltd2.png",
+        "/assets/images/resources/voddic-ltd2.png"
       ])
     )
   );
+});
+
+self.addEventListener("fetch", e => {
+  if (!e.request.url.includes("192.168.4.1")) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request))
+    );
+  }
 });
